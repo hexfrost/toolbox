200b2f3684a1757ce21d426f6f24d0a4a035f807
Ilia Kaziamov <kaziamov@outlook.com>
2025-05-21 09:36:44 +0400

    update tests for debug client

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -4,7 +4,7 @@ from tests.fixtures.database import temp_db, db_settings
 from tests.fixtures.db_connect import database_connector
 
 
-async def test_fastapi_depends_itegration_test_2(temp_db, db_settings, database_connector):
+async def test_fastapi_depends_itegration_test(temp_db, db_settings, database_connector):
 
     from fastapi import Depends, FastAPI
     app = FastAPI()
@@ -17,3 +17,14 @@ async def test_fastapi_depends_itegration_test_2(temp_db, db_settings, database_
     async with debug_client(app) as client:
         response1 = await client.get('/')
         assert response1.status_code == 200
+
+
+async def test_debug_client_negative_test(temp_db, db_settings, database_connector):
+
+    from fastapi import FastAPI
+    app = FastAPI()
+
+    from toolbox.testing import debug_client
+    async with debug_client(app) as client:
+        response1 = await client.get('/')
+        assert response1.status_code == 404
