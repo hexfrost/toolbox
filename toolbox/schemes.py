aaec8b5e47046d9f579dd867f419da2a024cf6c5
Ilia Kaziamov <kaziamov@outlook.com>
2025-05-22 20:35:41 +0400

    update sensitive models

--- toolbox/schemes.py
+++ toolbox/schemes.py
@@ -5,6 +5,9 @@ from pydantic import BaseModel
 from toolbox.cipher import CipherSuiteManager
 
 
+SensitiveFieldData = str | bytes
+
+
 class BaseScheme(BaseModel):
     pass
 
@@ -18,13 +21,16 @@ class SensitiveDataScheme(BaseScheme):
 
     @property
     def cipher_suite_manager(self) -> CipherSuiteManager:
-        if SensitiveDataScheme._cipher_suite_manager:
-            return SensitiveDataScheme._cipher_suite_manager
-        raise RuntimeError("cipher_suite_manager not set")
+        return SensitiveDataScheme._cipher_suite_manager
+
+    def get_cipher_suite_manager(self):
+        if not self._cipher_suite_manager:
+            raise RuntimeError("cipher_suite_manager not set")
+        return self._cipher_suite_manager
 
     @classmethod
     def set_cipher_suite_manager(cls, suite: CipherSuiteManager):
-        cls._cipher_suite_manager = suite
+        SensitiveDataScheme._cipher_suite_manager = suite
 
     def encrypt_fields(self):
         fields = self._sensitive_attributes
@@ -32,7 +38,7 @@ class SensitiveDataScheme(BaseScheme):
             if hasattr(self, field):
                 value = getattr(self, field)
                 if value is not None:
-                    encrypted_value = self._cipher_suite_manager.encrypt(value)
+                    encrypted_value: SensitiveFieldData = self.get_cipher_suite_manager().encrypt(value)
                     setattr(self, field, encrypted_value)
         return self
 
@@ -42,6 +48,6 @@ class SensitiveDataScheme(BaseScheme):
             if hasattr(self, field):
                 value = getattr(self, field)
                 if value is not None:
-                    decrypted_value = self._cipher_suite_manager.decrypt(value)
+                    decrypted_value: SensitiveFieldData = self.get_cipher_suite_manager().decrypt(value)
                     setattr(self, field, decrypted_value)
         return self
