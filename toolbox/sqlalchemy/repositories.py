f3e9eb0386c33a2f2b8d4b0a638d549f47a206d4
Ilia Kaziamov <kaziamov@outlook.com>
2025-05-22 20:34:28 +0400

    update repositories logic

--- toolbox/sqlalchemy/repositories.py
+++ toolbox/sqlalchemy/repositories.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import Depends
+from pydantic import BaseModel
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -16,15 +17,28 @@ class AbstractDatabaseCrudManager:
     _pydantic_model = type[AnyPydanticModel]
 
     @classmethod
-    async def add_one(cls, conn: AsyncSession, data_model: AnyPydanticModel) -> None:
-        obj = cls._alchemy_model(**data_model.encrypt_fields().model_dump())
+    def to_alchemy_model(cls, pydantic_model: AnyPydanticModel) -> AnySQLAlchemyModel:
+        encripted_data = pydantic_model.encrypt_fields()
+        obj = cls._alchemy_model(**encripted_data.model_dump())
+        return obj
+
+    @classmethod
+    def to_pydantic_model(cls, alchemy_model: AnySQLAlchemyModel) -> AnyPydanticModel:
+        new_model = cls._pydantic_model.model_validate(alchemy_model.__dict__)
+        decrypted_model = new_model.decrypt_fields()
+        return decrypted_model
+
+    @classmethod
+    async def add_one(cls, conn: AsyncSession, data_model: AnyPydanticModel) -> AnyPydanticModel:
+        obj = cls.to_alchemy_model(data_model)
         conn.add(obj)
         await conn.commit()
         await conn.refresh(obj)
         return obj
 
     @classmethod
-    async def get_all(cls, conn: AsyncSession, limit: int = 100, offset: int = 0):
+    async def get_all(cls, conn: AsyncSession, limit: int = 100, offset: int = 0) -> list[AnyPydanticModel]:
         query = select(cls._alchemy_model).limit(limit).offset(offset)
-        result = [v[0] for v in (await conn.execute(query)).all()]
+        raw_data = (await conn.execute(query)).all()
+        result = [cls.to_pydantic_model(v[0]) for v in raw_data]
         return result
